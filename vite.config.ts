fd55f7a7df26388597aabb0f89131d668b6b46ac
Abdalla Izzeldin <187668492+wdezzo-kak@users.noreply.github.com>
2026-03-11 01:47:45 +0200

    Add base path and alias resolution to Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import path from 'path'
 
 export default defineConfig({
+  base: '/school-web-app-demo',
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
   plugins: [react(), tailwindcss()],
 })
