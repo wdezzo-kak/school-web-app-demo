826e8130cde6dcf98d224aeed929012f049785e6
Abdalla Izzeldin <187668492+wdezzo-kak@users.noreply.github.com>
2026-03-11 01:57:54 +0200

    Update Vite config to load environment variables

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,25 @@
-import { defineConfig } from 'vite'
+import { defineConfig , loadEnv} from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import path from 'path'
 
-export default defineConfig({
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export default defineConfig(({mode}) =>{
+  const env = loadEnv(mode, '.', '');
+  return {
   base: '/school-web-app-demo',
+  server: {
+        port: 3000,
+        host: '0.0.0.0',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
   },
+    
   plugins: [react(), tailwindcss()],
-})
+  };
+});
+
